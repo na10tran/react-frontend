afb21d6d57f3c351847790c1f4dc22fa39939466
Nathan Tran <90416892+na10tran@users.noreply.github.com>
2022-04-11 01:08:22 -0700

    Connecting front end to back end

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,20 +1,56 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
+
 
 function MyApp() { 
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character,i) => {
+    const updated = characters.filter((character, i) => {
       return i !== index
     });
     setCharacters(updated);
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    makePostCall(person).then(result =>{
+      if(result && result.status === 200)    
+        setCharacters([...characters, person]);
+    });
+
   }
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch(error) {
+      //we're not handling errors. just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+  
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:5000/users' , person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, [] );  
 
   return (
     <div className="container">
